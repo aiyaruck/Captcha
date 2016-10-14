0b265d0ce35b9ffb6f2785ea584ea9595ddac08d
Your Name <you@example.com>
2016-10-14 17:04:39 +0700

    LF แก้ไข

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -12,17 +12,49 @@ function Operator(inputOP) { //for convert int to Operator
     }
 }
 
- function Leftoperand(c){
- if (c===0) return '0';
- else if (c===1)return '1';
-else if (c===2)return '2';
-else if (c===3)return '3';
-else if (c===4)return '4';
-else if (c===5)return '5';
-else if (c===6)return '6';
-else if (c===7)return '7';
-else if (c===8)return '8';
-else if (c===9)return '9';
+function leftOperator(inputLF) { // Integer Operator String
+   this.toInteger = function() {
+       if (inputLF === 0) {
+           return '0'
+       } else if (inputLF === 1) {
+           return '1'
+       } else if (inputLF === 2) {
+           return '2'
+       } else if (inputLF === 3) {
+           return '3'
+       } else if (inputLF === 4) {
+           return '4'
+       } else if (inputLF === 5) {
+           return '5'
+       } else if (inputLF === 6) {
+           return '6'
+       } else if (inputLF === 7) {
+           return '7'
+       } else if (inputLF === 😎 {
+           return '8'
+       } else if (inputLF === 9) {
+           return '9'
+       }
+   }
+}
 }
 
 //Test Captcha
+describe("Test Operator", function() {
+    it("inputOP = 1, Operator = +", function() {
+        var test = new Operator(1);
+        expect(test.toOperator()).toEqual('+');
+    });
+    it("inputOP = 2, Operator = -", function() {
+        var test = new Operator(2);
+        expect(test.toOperator()).toEqual('-');
+    });
+    it("inputOP = 3, Operator = *", function() {
+        var test = new Operator(3);
+        expect(test.toOperator()).toEqual('*');
+    });
+    it("inputOP = 4, Operator = /", function() {
+        var test = new Operator(4);
+        expect(test.toOperator()).toEqual('/');
+    });
+});
